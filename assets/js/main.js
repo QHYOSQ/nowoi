bce06298606ba574526ed2a1d9b79696d7ef9f60
QHYOSQ <qhyosq@gmail.com>
2026-02-24 14:52:08 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,12 @@
     year.textContent = new Date().getFullYear();
   }
 })();
+
+
+
+// Footer year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// ===== Under construction switch =====
+// 网站正式上线时，取消下面一行的注释即可
+// document.getElementById("underConstructionOverlay").style.display = "none";
